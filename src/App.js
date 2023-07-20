30836d0f295f4dd2f432feae0a3b678665a830f1
Jazib Rehman <jazibrehman30@gmail.com>
2023-07-20 13:49:28 -0700

    changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
-import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 import { useEffect } from "react";
 import PublicRoutes from "./routes/PublicRoutes";
 import ProtectedRoutes from "./routes/ProtectedRoutes";
